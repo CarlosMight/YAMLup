664f482b2272cb36b1465bd020bc07148d3be7f4
CarlosMight <carlosmight@gmail.com>
2018-01-08 00:01:57 -0800

    Adds YAML Frontmatter editor

--- src/layouts/Sandbox.vue
+++ src/layouts/Sandbox.vue
@@ -1,12 +1,36 @@
 <template lang="pug">
+  div.full-height
+    codemirror(
+      ref='editor'
+      v-model='yaml'
+      :options='codemirrorOpts'
+    )
 </template>
 
 <script>
+  import {codemirror} from 'vue-codemirror'
+  require('codemirror/lib/codemirror.css')
+  require('codemirror/mode/yaml-frontmatter/yaml-frontmatter.js')
+  require('codemirror/mode/gfm/gfm.js')
+
   export default {
-    name: 'layout-sandbox'
+    name: 'layout-sandbox',
+    components: {codemirror},
+
+    data () {
+      return {
+        yaml: '',
+        codemirrorOpts: {
+          mode: 'yaml-frontmatter',
+          base: 'gfm',
+          lineWrapping: true
+        }
+      }
+    }
   }
 </script>
 
 <style lang="sass">
-  @import "./src/assets/sass/main"
+  .vue-codemirror, .CodeMirror
+    height: 100%
 </style>
